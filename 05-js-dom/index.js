f3026ed8c25afbf8c4f3af534da98dd33493c440
frantzmoseau <frantzmoseau35@gmail.com>
2022-07-21 20:24:40 -0400

    done

--- 05-js-dom/index.js
+++ 05-js-dom/index.js
@@ -29,6 +29,31 @@ const pokemon = {
 // Changing Dom Elements and Creating Dom Elements
 // =============================================================================
 
+pokemons.forEach(pokemon) => renderAPokeCard(+
+  )
+function renderAPokeCard(pokemonObl) {
+  const li = document.createElement("li");
+  const h3 = document.createElement('h3');
+  const img =document.createElement("img");
+  const p = document.createElement("p");
+
+  // const pokemon = {
+  //   name: "bulbasaur",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  //   weight: 69,
+  // };
+  // pokeTitle - innerText
+
+  //pokeImg
+
+  //
+   li.classList.add("poke_card,bounce");
+   h3.classList.add("poke__header");
+   img.classList.add('poke__img')
+   li.appendChild(h3, img, p);
+   debugger;
+}
 // =============================================================================
 // Removing Dom Elements
 // =============================================================================
