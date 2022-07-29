e044720046ea858da061b10d8220f988bbe11a11
Eriberto Guzman <eri.guzman@flatironschool.com>
2022-07-28 20:51:40 -0400

    Update lecture

--- 07-async-js-Fetch/index.js
+++ 07-async-js-Fetch/index.js
@@ -1,5 +1,5 @@
 const firstInput = document.querySelector("input")
-pokemons.forEach((pokemon) => renderAPokeCard(pokemon))
+
 
 function renderAPokeCard(pokemonObj){
 
@@ -25,7 +25,7 @@ function renderAPokeCard(pokemonObj){
   // add text for button "remove"
   button.innerText = "remove" // <button>remove</button>
 
-  img.src = pokemonObj.frontImg
+  img.src = pokemonObj.backImg
   img.alt = pokemonObj.name
   img.width = 150
 
@@ -70,12 +70,43 @@ document.addEventListener("keydown", (event) => {
 
 // Bonus: Make the picture change when mouseenter, then zoom out when mouseleaves
 // =============================================================================
+// WHERE TO ADD OUR FIRST EVENT LISTENER FOR MOUSE ENTER
+document.querySelector(".poke__img").parentElement.remove()
+const imgList = document.querySelectorAll(".poke__img")
 
+// Have a nodeList, now we need to add an event listener to EACH item in that list
+
+imgList.forEach((image) => {
+  image.addEventListener('mouseenter', (event) => {
+    const foundPokeObj = pokemons.find((poke) => event.target.alt === poke.name)
+    event.target.src = foundPokeObj.frontImg
+  })
+
+  image.addEventListener('mouseleave', (event) => {
+    const foundPokeObj = pokemons.find((poke) => event.target.alt === poke.name)
+    event.target.src = foundPokeObj.backImg
+  })
+})
 
 // FETCH AND PROMISE
 // =============================================================================
+// FETCH MAKES BY DEFAULT A GET REQUEST
+// FETCH IS ALWAYS RETURN A PROMISE
+fetch("http://localhost:3000/pokemons") // GIVES US BACK A PROMISE
+  .then((resp) => resp.json()) // GIVE US BACK A PROMISE
+  .then((data) => {
+    data.forEach((pokemon) => renderAPokeCard(pokemon))
+  })
+  // .then handles that promise and waits for the resp
+  // resp.json() converts our resp into JS 
 
 
+// SYNCHRONOUS - An operataion takes place sequentially. 
+// TECHNICAL TERM!!!
+// ASYNCHRONOUS - When we do something that is going to take a 
+// long time. 
+// we continue with everything else, and let the 
+// other thing run on its own
 
 
 
